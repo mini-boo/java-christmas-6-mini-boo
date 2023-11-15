35d5a8e513ec980e53542d60a74147a05d854023
Hyunsoo Kim <sgn06221@gmail.com>
2023-11-15 12:02:24 +0900

    [refactor] apllication run 메소드 수정

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,13 @@
 package christmas;
 
+import christmas.Controller.ChristmasController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        ChristmasController christmasController = new ChristmasController();
+
+        christmasController.run();
     }
 }
